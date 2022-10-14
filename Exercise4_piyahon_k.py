18db6b8a722bf7b71b3801bf3084efc41bec9255
piyachon-k <105402061+piyachon-k@users.noreply.github.com>
2022-10-14 09:30:09 +0700

    Add files via upload

---
+++ Exercise4_piyahon_k.py
@@ -0,0 +1,10 @@
+fo_eng = 70.3
+gen_bus = 92.1
+int_com_sys = 83.2
+com_prog =79.6
+
+print('-- Your Score --')
+print(f'Foundation English : {fo_eng}')
+print(f'General Business : {gen_bus}')
+print(f'Introduction to Computer Systems : {int_com_sys}')
+print(f'Computer Programming : {com_prog}')
